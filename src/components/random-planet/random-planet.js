4d79d640253d40b0e5067c52dd4e14c59d5bc0ba
fedka <fedkakatorjniy@gmail.com>
2019-05-26 17:29:00 +0300

    add error meaasge

--- src/components/random-planet/random-planet.js
+++ src/components/random-planet/random-planet.js
@@ -2,32 +2,93 @@ import React, { Component } from 'react';
 
 import './random-planet.css';
 
+import Spinner from '../spinner';
+import ErrorIndicator from '../error-indicator';
+import SwapiService from '../../services/swapi-service.js';
+
 export default class RandomPlanet extends Component {
 
+  swapiService = new SwapiService();
+
+  state = {
+    planet: {},
+    loading: true 
+  };
+
+  constructor(){
+    super();
+    this.updatePlanet();
+  };
+
+  onPlanetLoaded = (planet) => {
+    this.setState({
+      planet, 
+      loading: false,
+      error: false
+    });
+  };
+
+  onError = (error) => {
+    this.setState({
+      error: true,
+      loading: false
+    });
+  };
+
+  updatePlanet() {
+    const id = Math.floor(Math.random()*25) + 2;
+    // const id = 12000;
+    this.swapiService
+      .getPlanet(id)
+      .then(this.onPlanetLoaded)
+      .catch(this.onError);
+  };
+
   render() {
+    const { planet, loading, error } = this.state;
+    
+    const hasData = !(loading || error);
+
+    const errorMessage = error ? <ErrorIndicator /> : null;
+    const spinner = loading ? <Spinner/> : null;
+    const content = hasData ? <PlanetView planet={planet}/> : null;
+
     return (
       <div className="random-planet jumbotron rounded">
+        {errorMessage}
+        {spinner}
+        {content}
+      </div>
+    );
+  };
+}
+
+const PlanetView = ({planet}) => {
+
+  const { id, name, population, 
+    rotationPeriod, diameter } = planet;
+
+  return (
+    <React.Fragment>
         <img className="planet-image"
-             src="https://starwars-visualguide.com/assets/img/planets/5.jpg" />
+             src={`https://starwars-visualguide.com/assets/img/planets/${id}.jpg`} />
         <div>
-          <h4>Planet Name</h4>
+          <h4>{name}</h4>
           <ul className="list-group list-group-flush">
             <li className="list-group-item">
               <span className="term">Population</span>
-              <span>123124</span>
+              <span>{population}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Rotation Period</span>
-              <span>43</span>
+              <span>{rotationPeriod}</span>
             </li>
             <li className="list-group-item">
               <span className="term">Diameter</span>
-              <span>100</span>
+              <span>{diameter}</span>
             </li>
           </ul>
         </div>
-      </div>
-
-    );
-  }
-}
+    </React.Fragment>
+  );
+};
