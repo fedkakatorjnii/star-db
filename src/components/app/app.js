bf93385a87b84daa7683bcb46a108d1e2b2c5c78
fedka <ar-iv@mail.ru>
2019-06-04 22:54:48 +0300

    change css

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -35,12 +35,14 @@ export default class App extends Component {
           { planet }
 
           <div className="row mb2 button-row">
-            <button
-              className="toggle-planet btn btn-warning btn-lg"
-              onClick={this.toggleRandomPlanet}>
-              Toggle Random Planet
-            </button>
-            <ErrorButton />
+            <div className="col-md-12">
+              <button
+                className="toggle-planet btn btn-warning btn-lg"
+                onClick={this.toggleRandomPlanet}>
+                Toggle Random Planet
+              </button>
+              <ErrorButton />
+            </div>
           </div>
 
           <PeoplePage />
