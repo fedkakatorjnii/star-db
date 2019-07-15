b6c9344292a55fa9a61351707e7e5ecfe4255048
fedka <fedkakatorjniy@gmail.com>
2019-07-15 16:11:11 +0300

    Updates

---
+++ precache-manifest.97bba68ef10ed9c297ceae6eb21a7b21.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7bf13f7c566c25feccc2cc1d3361e04",
+    "url": "/star-gb/index.html"
+  },
+  {
+    "revision": "ca11991c7d77db87c0c6",
+    "url": "/star-gb/static/css/main.730b47d4.chunk.css"
+  },
+  {
+    "revision": "0d41e9757a58d7f20e07",
+    "url": "/star-gb/static/js/2.648ab61d.chunk.js"
+  },
+  {
+    "revision": "ca11991c7d77db87c0c6",
+    "url": "/star-gb/static/js/main.bbdf8f21.chunk.js"
+  },
+  {
+    "revision": "8cc4f5efe42f2bbc168e",
+    "url": "/star-gb/static/js/runtime~main.428489f3.js"
+  }
+]);
